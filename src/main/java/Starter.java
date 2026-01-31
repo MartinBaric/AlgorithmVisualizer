f697af9263c0230949d863119200a49c81158339
Martin B. <uajew2@gmail.com>
2026-01-31 21:53:11 +0100

    GUI initialization

---
+++ src/main/java/Starter.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
