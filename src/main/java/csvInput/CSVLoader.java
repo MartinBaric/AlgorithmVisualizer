465f266db942d854aebcf6de2ee7f0eb64f5848b
Martin B. <uajew2@gmail.com>
2026-02-01 12:51:01 +0100

    CSV Loader improved

--- src/main/java/csvInput/CSVLoader.java
+++ src/main/java/csvInput/CSVLoader.java
@@ -5,25 +5,28 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class CSVLoader {
     public static int[] loadCSVInput() {
-        try (Reader reader = Files.newBufferedReader(Path.of("data.csv"));
-             CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT.withIgnoreSurroundingSpaces())) {
+        try (InputStream is = CSVLoader.class.getResourceAsStream("/data.csv")) {
+            assert is != null;
+            try (CSVParser parser = new CSVParser(new InputStreamReader(is),
+                    CSVFormat.DEFAULT.withIgnoreSurroundingSpaces())) {
 
-            List<Integer> integerList = new ArrayList<>();
+                List<Integer> integerList = new ArrayList<>();
 
-            for (CSVRecord record : parser) {
-                integerList.add(Integer.parseInt(record.get(0)));
+                for (CSVRecord record : parser) {
+                    integerList.add(Integer.parseInt(record.get(0)));
+                }
+
+                return integerList.stream().mapToInt(Integer::intValue).toArray();
             }
-
-            return integerList.stream().mapToInt(Integer::intValue).toArray();
         } catch (IOException e) {
             Logger.getAnonymousLogger().info(e.getMessage());
         }
