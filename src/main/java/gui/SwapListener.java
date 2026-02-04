b4f896921a444066b5b0dcdfe1bef3ecd7debc62
Martin B. <uajew2@gmail.com>
2026-02-04 01:08:45 +0100

    Type removed

--- src/main/java/gui/SwapListener.java
+++
@@ -1,6 +0,0 @@
-package gui;
-
-@FunctionalInterface
-public interface SwapListener<T extends Comparable<T>> {
-    void swap(int i, int j);
-}
---
+++ src/main/java/sortingAlgorithms/SwapListener.java
@@ -0,0 +1,6 @@
+package sortingAlgorithms;
+
+@FunctionalInterface
+public interface SwapListener {
+    void swap(int i, int j);
+}
