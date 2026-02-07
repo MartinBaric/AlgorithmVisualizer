5a54a586a6ce9e8c583fe632d50fe8c6919664a7
Martin B. <uajew2@gmail.com>
2026-02-07 01:09:47 +0100

    parametrized tests

--- src/test/java/algorithms/AlgorithmTest.java
+++ src/test/java/algorithms/AlgorithmTest.java
@@ -1,8 +1,11 @@
 package algorithms;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import sortingAlgorithms.BubbleSort;
+import sortingAlgorithms.InsertionSort;
+import sortingAlgorithms.SelectionSort;
 import sortingAlgorithms.SortingAlgorithm;
 
 import java.util.ArrayList;
@@ -11,12 +14,18 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlgorithmTest {
-    @Test
-    public void BubbleSortTest() {
+    static List<SortingAlgorithm<Integer>> algorithms() {
+        return List.of(new SelectionSort<>(),
+                new BubbleSort<>(),
+                new InsertionSort<>());
+    }
+    @ParameterizedTest
+    @MethodSource("algorithms")
+    public void SortTest(SortingAlgorithm<Integer> sortingAlgorithm) {
         List<Integer> numbers = new ArrayList<>(List.of(7,8,5,1,4));
         List<Integer> expected = List.of(1,4,5,7,8);
-        SortingAlgorithm<Integer> bubbleSort = new BubbleSort<>(numbers);
-        bubbleSort.sort(null, null);
-        assertEquals(numbers, expected);
+        sortingAlgorithm.setList(numbers);
+        sortingAlgorithm.sort();
+        assertEquals(expected, numbers);
     }
 }
