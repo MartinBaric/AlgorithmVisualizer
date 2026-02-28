a40180c67914c03f9221e30affb62e642eb219d4
Martin B. <uajew2@gmail.com>
2026-02-28 18:38:05 +0100

    removed timeline, added lombok for noArgsConst

--- src/main/java/controller/SortingController.java
+++ src/main/java/controller/SortingController.java
@@ -1,46 +1,40 @@
 package controller;
 
 import animation.SortingAnimation;
-import javafx.animation.Timeline;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Toggle;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Rectangle;
+import lombok.NoArgsConstructor;
 import sortingAlgorithms.SortingAlgorithm;
-import sortingAlgorithms.enums.SortingType;
+import sortingAlgorithms.enums.SortingAlgorithmType;
 import sortingAlgorithms.functionalInterfaces.HighlightListener;
 import sortingAlgorithms.functionalInterfaces.SwapListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
 public class SortingController {
-    public SortingController() {
-
-    }
 
     public void startSortingAlgorithm(Toggle algorithmToggle, List<Integer> numbers,
                                       HBox barsContainer, Slider slider) {
-        Timeline timeline = new Timeline();
-        timeline.setCycleCount(1);
 
         List<Rectangle> bars = new ArrayList<>(barsContainer.getChildren().stream()
                 .map(r -> (Rectangle) r)
                 .toList());
 
-        SortingAnimation sortingAnimation = new SortingAnimation(bars, slider, timeline);
+        SortingAnimation sortingAnimation = new SortingAnimation(bars, slider);
 
         HighlightListener highlightListener = sortingAnimation.createHighlightListener();
         SwapListener swapListener = sortingAnimation.createSwapListener();
 
-        SortingType sortingType = (SortingType) algorithmToggle.getUserData();
-        SortingAlgorithm<Integer> sortingAlgorithm = sortingType.create(numbers, highlightListener, swapListener);
+        SortingAlgorithmType sortingAlgorithmType = (SortingAlgorithmType) algorithmToggle.getUserData();
+        SortingAlgorithm<Integer> sortingAlgorithm = sortingAlgorithmType.create(numbers, highlightListener, swapListener);
 
         sortingAlgorithm.sort();
 
-        sortingAnimation.dehighlightAll(timeline, bars, slider);
-
-        timeline.rateProperty().bind(slider.valueProperty());
-        timeline.play();
+        sortingAnimation.dehighlightAll();
+        sortingAnimation.play();
     }
 }
